be9911eec22bd10402e9ced9d9d40524c74a447d
Brendan Horst <yoshistar2000@gmail.com>
2021-03-08 08:02:01 -0500

    Added genre filter and fixed autoincrement in schema.sql

--- project/data.sql
+++ project/data.sql
@@ -3,4 +3,16 @@
 INSERT INTO games (title)
 VALUES ('Super Smash Bros'),
        ('Xenoblade Chronicles'),
-       ('Minecraft');
+       ('Minecraft'),
+       ('Terraria');
+
+INSERT INTO genres (name)
+VALUES ('Fighting'),
+       ('JRPG'),
+       ('Sandbox');
+
+INSERT INTO games_genres (game_id, genre_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 3);
--- project/games.py
+++ project/games.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from flask import Blueprint, g, render_template
+from flask import Blueprint, g, render_template, request
 
 from project.db import get_db
 
@@ -9,9 +9,24 @@ bp = Blueprint('games', __name__)
 @bp.route('/', methods=('GET', 'POST'))
 def game_list():
 
+    genre = "0"
+
+    if request.method == 'POST':
+
+        genre = request.form['genre']
+        print(genre)
+
     db = get_db()
 
-    games = db.execute('SELECT * FROM games').fetchall()
+    if genre != "0":
+        games = db.execute(
+            'SELECT title FROM games JOIN games_genres ON games.id = game_id WHERE genre_id = ?', genre
+            ).fetchall()
+
+    else:
+        games = db.execute(
+            'SELECT title FROM games'
+            ).fetchall()
 
     genres = db.execute('SELECT * FROM genres').fetchall()
 
