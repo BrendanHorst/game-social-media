7af76b9bddbed47b85d0373f989b8db17b72979a
Ethan Kubistek <ekubistek@gmail.com>
2021-03-12 10:56:40 -0500

    fixing register code

--- project/auth.py
+++ project/auth.py
@@ -18,34 +18,40 @@ def register():
 
     msg = ''
 
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-
+    if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-    elif request.method == 'POST':
-
         msg = 'Please fill out the form!'
+        db = get_db()
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+        msg = 'You have successfully registered!'
+
 
     return render_template('register.html', msg=msg)
 
 
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
 
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
 
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
             msg = 'You have successfully registered!'
 
 @bp.route('/login', methods=('GET', 'POST'))
